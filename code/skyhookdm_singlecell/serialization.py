ca8df0ee6b1abb5b1cbc0d5bc3591be7458816da
Aldrin <akmontan@ucsc.edu>
2020-03-12 09:54:32 -0700

    Fixed some issues with serialization where FB_Meta module was imported, and it wasn't clear when trying to use `FB_Meta.GetRootAsFB_Meta()` what the issue was. Imports are re-organized for the above reason. Also added a thin wrapper around FB_Meta that provides an interface to deserialize a FB_Meta binary into an arrow format using `pyarrow.ipc.open_stream()`

--- code/skyhookdm_singlecell/serialization.py
+++ code/skyhookdm_singlecell/serialization.py
@@ -1,8 +1,50 @@
 import logging
 
 import flatbuffers
+import pyarrow
 
-from skyhookdm_singlecell.Tables import FB_Meta
+# classes
+from skyhookdm_singlecell.Tables.FB_Meta import FB_Meta
+
+# functions
+from skyhookdm_singlecell.Tables.FB_Meta import (FB_MetaStart, FB_MetaEnd,
+                                                 FB_MetaAddBlobFormat,
+                                                 FB_MetaAddBlobData,
+                                                 FB_MetaAddBlobSize,
+                                                 FB_MetaAddBlobDeleted,
+                                                 FB_MetaAddBlobOrigOff,
+                                                 FB_MetaAddBlobOrigLen,
+                                                 FB_MetaAddBlobCompression)
+
+class SkyhookFlatbuffer(object):
+    logger = logging.getLogger('{}.{}'.format(__module__, __name__))
+    logger.setLevel(logging.INFO)
+
+    @classmethod
+    def from_binary(cls, flatbuffer_binary, offset=0):
+        return cls(FB_Meta.GetRootAsFB_Meta(flatbuffer_binary, offset))
+
+    def __init__(self, flatbuffer_obj, **kwargs):
+        super().__init__(**kwargs)
+
+        self.fb_obj = flatbuffer_obj
+
+    def is_arrow_type(self):
+        return self.fb_obj.BlobFormat() == 1
+
+    def get_size(self):
+        return self.fb_obj.BlobDataLength()
+
+    def get_data_as_arrow(self):
+        data_blob          = self.fb_obj.BlobDataAsNumpy()
+
+        stream_reader      = pyarrow.ipc.open_stream(data_blob.tobytes())
+        deserialized_table = pyarrow.Table.from_batches(
+            list(stream_reader),
+            schema=stream_reader.schema
+        )
+
+        return deserialized_table
 
 class SkyhookFlatbufferGeneExpression(object):
     logger = logging.getLogger('{}.{}'.format(__module__, __name__))
@@ -10,7 +52,7 @@ class SkyhookFlatbufferGeneExpression(object):
 
     @classmethod
     def from_arrow_buffer(cls, arrow_buffer):
-        return cls(arrow_buffer.size).wrap_arrow_buffer(arrow_buffer)
+        return cls(initial_byte_count=arrow_buffer.size).wrap_arrow_buffer(arrow_buffer)
 
     def __init__(self, initial_byte_count=0, **kwargs):
         super().__init__(**kwargs)
@@ -18,42 +60,24 @@ class SkyhookFlatbufferGeneExpression(object):
         self.initial_byte_count = initial_byte_count
         self.builder            = flatbuffers.Builder(initial_byte_count)
 
-    def serialize_records(self, arrow_buffer):
-        """
-        Write element vector, in reverse order
-        """
-
-        FB_Meta.FB_MetaStartBlobDataVector(self.builder, arrow_buffer.size)
-
-        for arrow_buffer_byte in reversed(arrow_buffer):
-            self.builder.PrependByte(arrow_buffer_byte)
-
-        return builder.EndVector(arrow_buffer.size)
-
     def wrap_arrow_buffer(self, arrow_buffer):
         """
         Write object, in-order
         """
 
         # we want to serialize the data first (build vector from back to front)
-        wrapped_data_blob = self.serialize_records(arrow_buffer)
+        wrapped_data_blob = self.builder.CreateByteVector(arrow_buffer.to_pybytes())
 
-        FB_Meta.FB_MetaStart(self.builder)
+        FB_MetaStart(self.builder)
 
-        FB_Meta.FB_MetaAddBlobFormat(self.builder, 1)
-        FB_Meta.FB_MetaAddBlobData(self.builder, wrapped_data_blob)
-        FB_Meta.FB_MetaAddBlobSize(self.builder, arrow_buffer.size)
-        FB_Meta.FB_MetaAddBlobDeleted(self.builder, False)
-        FB_Meta.FB_MetaAddBlobOrigOff(self.builder, 0)
-        FB_Meta.FB_MetaAddBlobOrigLen(self.builder, arrow_buffer.size)
-        FB_Meta.FB_MetaAddBlobCompression(self.builder, 0)
+        FB_MetaAddBlobFormat(self.builder, 1)
+        FB_MetaAddBlobData(self.builder, wrapped_data_blob)
+        FB_MetaAddBlobSize(self.builder, arrow_buffer.size)
+        FB_MetaAddBlobDeleted(self.builder, False)
+        FB_MetaAddBlobOrigOff(self.builder, 0)
+        FB_MetaAddBlobOrigLen(self.builder, arrow_buffer.size)
+        FB_MetaAddBlobCompression(self.builder, 0)
 
-        self.builder.Finish(FB_Meta.FB_MetaEnd(builder))
+        self.builder.Finish(FB_MetaEnd(self.builder))
 
         return self.builder.Output()
-
-    def deserialize(self, flatbuffer_obj, blob_byte_offset=0):
-        skyhook_blob_wrapper = FB_Meta.GetRootAsFB_Meta(flatbuffer_obj, blob_byte_offset)
-
-        skyhook_blob_wrapper.BlobDataLength()
-        skyhook_blob_wrapper.BlobFormat()
