31671432e7499af833c8c3528a417f3021ec9d4b
Aldrin <akmontan@ucsc.edu>
2020-04-10 03:15:08 +0000

    Added setup.py (generated by poetry build).
    This is useful so that containers can run pip install -e
    /path/to/pyproject to install. Specifically, poetry and pyenv are
    annoying to use in conjunction with ceph in a container (due to hacks to
    path python3-rados)

---
+++ python/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages     = ['skyhookdm_singlecell', 'skyhookdm_singlecell.Tables']
+package_data = { '': ['*'] }
+
+install_requires = [
+    'awscli          >= 1.17.15 , <  2.0.0',
+    'cython          >= 0.29.15 , < 0.30.0',
+    'flatbuffers     >= 1.11    , <   1.12',
+    'h5py            >= 2.10    , <    3.0',
+    'numpy           >= 1.17    , <    2.0',
+    'owlready2       >= 0.23    , <   0.24',
+    'pandas          >= 1.0.1   , <  2.0.0',
+    'pyarrow         >= 0.15.0  , < 0.16.0',
+    'python-cephlibs >= 0.94.5  , < 0.95.0',
+    'scipy           >= 1.3     , <    2.0',
+]
+
+setup_kwargs = {
+    'name'            : 'skyhookdm-singlecell',
+    'version'         : '0.1.0',
+
+    'description'     : '',
+    'long_description': None,
+
+    'author'          : 'Aldrin Montana',
+    'author_email'    : 'akmontan@ucsc.edu',
+
+    'maintainer'      : None,
+    'maintainer_email': None,
+
+    'url'             : None,
+
+    'packages'        : packages,
+    'package_data'    : package_data,
+
+    'install_requires': install_requires,
+    'python_requires' : '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
