51a445332ed6b90e89c8e66ae199502149526966
Aldrin <akmontan@ucsc.edu>
2020-04-25 14:57:07 -0700

    set versions to 1 instead of 0, just because

--- python/skyhookdm_singlecell/__init__.py
+++ python/skyhookdm_singlecell/__init__.py
@@ -2,6 +2,6 @@
 __version__ = '0.1.0'
 
 # Some skyhook depdency metadata; but currently dummy values
-__skyhook_version__             = 0
-__skyhook_data_schema_version__ = 0
-__skyhook_data_struct_version__ = 0
+__skyhook_version__             = 1
+__skyhook_data_schema_version__ = 1
+__skyhook_data_struct_version__ = 1
