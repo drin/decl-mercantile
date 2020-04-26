10ed01b1c7ed58cf432fdd6c474721439a0fa773
akmontan <akmontan@ucsc.edu>
2020-04-26 14:17:56 -0500

    Adding script that runs toolbox/rados-write
    This is a short-term solution to using rados-write on many directories.

---
+++ python/scripts/load.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+
+for workload_dir in (find /mnt/sdb/data/workload/simulated -mindepth 1 -maxdepth 1 -type d)
+    set num_files (ls -1 $workload_dir | wc -l)
+    set table_name (basename $workload_dir)
+
+    # echo "$table_name ($num_files)"
+    toolbox/rados-write --config-file $HOME/cluster/ceph.conf --input-dir $workload_dir  --data-format arrow --use-skyhook-wrapper --pool "simulated" --table $table_name
+end
+
