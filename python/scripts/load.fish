5acac3e21b6416dbcad781aff4c3c821eece80b6
akmontan <akmontan@ucsc.edu>
2020-05-03 17:45:39 -0500

    Extended load.fish to use a set of batch sizes
    Each batch size determines the number of columns in a partition, so
    load.fish is essentially the load-step of the benchmark

--- python/scripts/load.fish
+++ python/scripts/load.fish
@@ -1,10 +1,13 @@
 #!/usr/bin/fish
 
-for workload_dir in (find /mnt/sdb/data/simulated -mindepth 1 -maxdepth 1 -type d)
-    set num_files (ls -1 $workload_dir | wc -l)
-    set table_name (basename $workload_dir)
+set batch_sizes 100 1000 10000
 
-    # echo "$table_name ($num_files)"
-    toolbox/rados-write --config-file $HOME/cluster/ceph.conf --input-dir $workload_dir  --data-format arrow --use-skyhook-wrapper --pool "simulated" --table $table_name
+for batch_size in $batch_sizes
+    for workload_dir in (find /mnt/sdb/data/simulated-arrow-15/"batch-$batch_size" -mindepth 1 -maxdepth 1 -type d)
+        set num_files (ls -1 $workload_dir | wc -l)
+        set table_name (basename $workload_dir)
+
+        # echo "$table_name ($num_files)"
+        toolbox/rados-write --config-file $HOME/cluster/ceph.conf --input-dir $workload_dir  --data-format arrow --use-skyhook-wrapper --pool "simulated" --table "$table_name-$batch_size"
+    end
 end
-
