f59fce3b325faecbd90b86a9ac4e5a31021bf502
akmontan <akmontan@client0.singlecell-demo.skyhook-pg0.wisc.cloudlab.us>
2020-05-12 10:18:42 -0500

    Added loop for batch size

--- python/scripts/load.fish
+++ python/scripts/load.fish
@@ -2,12 +2,20 @@
 
 set batch_sizes 100 1000 10000
 
+#set base_search_dir /mnt/sdb/data/simulated-arrow-15/"batch-$batch_size"
+set base_search_dir "/mnt/sdb/data/cost-model"
+set pool_name       "cost-model"
+
 for batch_size in $batch_sizes
-    for workload_dir in (find /mnt/sdb/data/simulated-arrow-15/"batch-$batch_size" -mindepth 1 -maxdepth 1 -type d)
+    set search_dir "$base_search_dir/batch-$batch_size"
+
+    for workload_dir in (find "$search_dir" -mindepth 1 -maxdepth 1 -type d)
         set num_files (ls -1 $workload_dir | wc -l)
         set table_name (basename $workload_dir)
 
-        # echo "$table_name ($num_files)"
-        toolbox/rados-write --config-file $HOME/cluster/ceph.conf --input-dir $workload_dir  --data-format arrow --use-skyhook-wrapper --pool "simulated" --table "$table_name-$batch_size"
+        echo "$table_name-$batch_size ($num_files)"
+        #echo "$ceph_opts"
+
+        toolbox/rados-write --config-file $HOME/cluster/ceph.conf --input-dir $workload_dir --data-format arrow --use-skyhook-wrapper --pool $pool_name --table $table_name-$batch_size
     end
 end
